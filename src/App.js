b86753cfa41f58ab9ab9131c545471555acf9ef1
unknown <giangtx13@gmail.com>
2023-03-18 09:33:06 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+
+const App = () => {
+
+  return (
+    <div>duong duong</div>
+  )
+
+  // return (
+  //   <Router>
+  //     <CheckUrl>
+  //       <Switch>
+  //       </Switch>  
+  //     </CheckUrl>
+  //   </Router>
+  // );
+}
+
+export default App;
